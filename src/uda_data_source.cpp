7f4e714b503be7194b1a720378fd94ce7330af63
sdixon <stephen.dixon@ukaea.uk>
2026-03-15 18:56:20 +0100

    fixing seg fault

--- src/uda_data_source.cpp
+++ src/uda_data_source.cpp
@@ -169,7 +169,7 @@ class ArrayBuilder
         m_data = db.data;
         m_size = db.data_n;
         m_shape.reserve(db.rank);
-        for (unsigned int i = db.rank - 1; i >= 0; --i) {
+        for (int i = db.rank - 1; i >= 0; --i) {
             m_shape.push_back(db.dims[i].dim_n);
         }
         m_data_type = db.data_type;
